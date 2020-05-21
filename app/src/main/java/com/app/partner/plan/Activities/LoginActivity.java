dc4747d6fc715112fa372b9c675413a8988da13d
adriantm26 <adriantorresm1997@gmail.com>
2020-05-21 10:09:58 -0500

    first commit

---
+++ app/src/main/java/com/app/partner/plan/Activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.app.partner.plan.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.partner.plan.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
