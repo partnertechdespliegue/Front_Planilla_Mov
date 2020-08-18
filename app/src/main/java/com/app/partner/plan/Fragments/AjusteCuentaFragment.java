af7507efecce52410286d58e202bccb7be07bd6f
Brighyt-emily <38229889+Brighyt-emily@users.noreply.github.com>
2020-08-18 14:15:03 -0500

    subir foto 1

--- app/src/main/java/com/app/partner/plan/Fragments/AjusteCuentaFragment.java
+++ app/src/main/java/com/app/partner/plan/Fragments/AjusteCuentaFragment.java
@@ -2,20 +2,33 @@ package com.app.partner.plan.Fragments;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.app.partner.plan.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 public class AjusteCuentaFragment extends Fragment {
     private EditText editNombre, editApellido, editCorreo;
+    ImageView foto;
+    Button btnCambiarFoto;
     Activity activity;
 
     public AjusteCuentaFragment() {
@@ -30,19 +43,47 @@ public class AjusteCuentaFragment extends Fragment {
         }
     }
 
-
-
-    private void obtenerViews(){
-      //  editNombre =
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_ajuste_cuenta, null);
 
-        obtenerViews();
-        return inflater.inflate(R.layout.fragment_ajuste_cuenta, container, false);
+        obtenerViews(view);
+        Cambiar();
+        return view;
+    }
+
+    private void obtenerViews(View view){
+        foto = view.findViewById(R.id.imageViewPefil);
+        btnCambiarFoto = view.findViewById(R.id.btnCambiar);
+    }
+
+    public void Cambiar(){
+        btnCambiarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               cargarImagen();
+            }
+        });
+
+    }
+
+    public void cargarImagen(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione"),2);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode== RESULT_OK){
+            Uri path = data.getData();
+            System.out.println(path);
+            foto.setImageURI(path);
+        }
+
     }
 }
